1284098785b45f8b7a4b35e50a2a843da55039df
shashank-jadhav <shashankjadhav08@gmail.com>
2018-07-26 12:35:31 +0000

    Add files via upload

---
+++ java/CrudDemo/src/main/java/com/shashank/service/BookService.java
@@ -0,0 +1,15 @@
+package com.shashank.service;
+
+import java.util.List;
+
+import com.shashank.model.Book;
+
+public interface BookService {
+
+	
+	public List<Book> getAllBooks();
+	
+	public Long saveBook(Book book);
+	
+	public Book getSingleBook(Long bookId);
+}
